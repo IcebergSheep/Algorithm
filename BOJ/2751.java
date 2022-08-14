632cbaa2ee0c87bd6154bfa066d6c21d471f243a
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-08-14 13:43:52 +0900

    Create 2751.java

---
+++ BOJ/2751.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        
+        int N;
+
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        N = scan.nextInt();
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = scan.nextInt();
+        }
+        Arrays.sort(arr);
+
+        for(int i = 0; i<N; i++){
+            sb.append(arr[i]+"\n");
+        }
+        System.out.println(sb);
+    }
+}
