124064fddc26f58ab25c31be54b0b3bbb76ec185
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-08-19 14:36:23 +0900

    Update 2740.java

--- BOJ/2740.java
+++ BOJ/2740.java
@@ -1,10 +1,18 @@
-public class Main {
+import java.util.Scanner;
+
+public class  {
     public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+        int K = scan.nextInt();
+
         //행렬 생성
         int[][] A = new int[N][M];
         int[][] B = new int[M][K];
         int[][] C = new int[N][K];
-        
+
         //곱셈 과정
         for(int i = 0; i<N; i++){
             for(int j = 0; j<K; j++){
@@ -13,6 +21,6 @@ public class Main {
                 }
             }
         }
-        
+
     }
 }
