0b4973e275b6e7bfd72278f360a55ad80d5e721a
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-08-19 18:53:44 +0900

    Update 11866.java

--- BOJ/11866.java
+++ BOJ/11866.java
@@ -1,18 +1,16 @@
-//요세푸스 문제
-
-import java.util.LinkedList;
+mport java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
+public class b11866 {
+    public static void Main(String[] args) {
+        
         int N,K;
         Scanner scan = new Scanner(System.in);
-        N = scan.nextInt();
-        K = scan.nextInt();
+        N = scan.nextInt(); //인원
+        K = scan.nextInt(); //순서
 
-        Queue<Integer> q = new LinkedList<>(); 
+        Queue<Integer> q = new LinkedList<>();
 
         for(int i = 1; i<=N; i++){
             q.add(i);
@@ -20,13 +18,13 @@ public class Main {
 
         System.out.print("<");
 
-        while(q.size()>0){
+        while(!q.isEmpty()){
             for(int i = 0; i<K-1; i++){
                 q.add(q.poll());
             }
             System.out.print(q.poll());
 
-            if(q.size()>0){
+            if(!q.isEmpty()){
                 System.out.print(", ");
             }
 
